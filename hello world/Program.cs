c2e0b937d016e4a11ec103a9cb6d4e64411c4189
AkshayKumarTN <tnak369@gmail.com>
2021-06-26 09:54:29 +0530

    [Akshay] update

--- hello world/Program.cs
+++ hello world/Program.cs
@@ -7,7 +7,8 @@ namespace hello_world
         static void Main(string[] args)
         {
             int num = 4;
-            Console.WriteLine(num);
+            Console.WriteLine(num*4);
+            Console.WriteLine(num / 2);
             Console.WriteLine("Hiii");
             Console.WriteLine("Hello World!");
         }
